0d163700ccc27db696e8357073767f1b3dadec4e
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2024-05-29 18:38:01 +0100

    more config because i deleted my last one

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,9 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+require("neo-tree").setup({
+  window = {
+    position = "right", -- Position the neo-tree window on the left
+    width = 40, -- Set the width of neo-tree window (Adjust as needed)
+  },
+})
