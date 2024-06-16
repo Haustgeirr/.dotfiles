68edd1af1a74ef260f9a8c677d224bd5913efec8
Haustgeirr <3540179+Haustgeirr@users.noreply.github.com>
2024-06-16 15:08:22 +0100

    wip: alacritty and tmux

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,13 @@
 require("config.lazy")
 
 require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+      hide_dotfiles = false,
+      hide_gitignored = true,
+    },
+  },
   window = {
     position = "right", -- Position the neo-tree window on the left
     width = 40, -- Set the width of neo-tree window (Adjust as needed)
--- .zshrc
+++ .zshrc
@@ -104,3 +104,6 @@ alias c='clear'
 
 # To customize prompt, run `p10k configure` or edit ~/.dotfiles/.p10k.zsh.
 # [[ ! -f ~/.dotfiles/.p10k.zsh ]] || source ~/.dotfiles/.p10k.zsh
+
+
+source ~/.aliases
